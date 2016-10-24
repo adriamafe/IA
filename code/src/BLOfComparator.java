2c2cffa6ad735aeebf1d7be31c30b5f52c0dae71
adriamafe <adriamf.martinez05@gmail.com>
2016-10-24 22:32:31 +0200

    Update BLOfComparator.java

--- code/src/BLOfComparator.java
+++ code/src/BLOfComparator.java
@@ -13,8 +13,6 @@ public class BLOfComparator implements Comparator<Oferta>{
     public int compare(Oferta o1, Oferta o2) {
         if(o1.getDias()<o2.getDias()) return -1;
         if(o1.getDias()>o2.getDias()) return 1;
-        if(o1.getPrecio()<o2.getPrecio()) return -1;
-        if(o1.getPrecio()>o2.getPrecio()) return 1;
         return 0;
     }
 }
