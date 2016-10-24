966fa9d6eece458d73370f9db31ddb5ffb7d7af5
adriamafe <adriamf.martinez05@gmail.com>
2016-10-24 22:32:58 +0200

    Update BLPaqComparator.java

--- code/src/BLPaqComparator.java
+++ code/src/BLPaqComparator.java
@@ -13,8 +13,6 @@ public class BLPaqComparator implements Comparator<Paquete>{
     public int compare(Paquete p1, Paquete p2) {
         if(p1.getPrioridad()<p2.getPrioridad()) return -1;
         if(p1.getPrioridad()>p2.getPrioridad()) return 1;
-        if(p1.getPeso()<p2.getPeso()) return 1;
-        if(p1.getPeso()>p2.getPeso()) return -1;
         return 0;
     }
 }
